435deaf14856a817f0f6fe5099008d393ff34a16
Kanako-muff <kanako.design00@gmail.com>
2023-02-20 02:06:18 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const button = document.getElementById('btn');
+
+function getJoke() {
+    fetch('https://official-joke-api.appspot.com/random_joke')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            const joke = data.setup + '\n' + data.punchline;
+            const robotShowText = document.getElementById('text-container');
+            // robotShowText.innerHTML = `<span>${joke}</span>`     ⬅︎【!】\n はhtml内では無効。
+            robotShowText.innerHTML = `<span>${data.setup}<br>${data.punchline}</span>`
+            setRobotSpeakText(joke);
+            speakText();
+        })
+}
+
+button.addEventListener('click', getJoke); //⬅︎ 【!】getJoke()じゃない。
+
+
+/* ---------------------- */
+/* functions for SPEAKING */
+/* ---------------------- */
+const robotSpeakText = new SpeechSynthesisUtterance();
+
+function setRobotSpeakText(text){
+    robotSpeakText.text = text;
+}
+
+function speakText(){
+    speechSynthesis.speak(robotSpeakText);
+}
