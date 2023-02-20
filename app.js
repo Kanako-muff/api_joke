43901c2ec4159684fb9d37e5ef4389fdb8eb16d5
Kanako-muff <kanako.design00@gmail.com>
2023-02-20 02:19:55 -0800

    second commit

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ function getJoke() {
     fetch('https://official-joke-api.appspot.com/random_joke')
         .then(res => res.json())
         .then(data => {
-            console.log(data);
             const joke = data.setup + '\n' + data.punchline;
             const robotShowText = document.getElementById('text-container');
             // robotShowText.innerHTML = `<span>${joke}</span>`     ⬅︎【!】\n はhtml内では無効。
